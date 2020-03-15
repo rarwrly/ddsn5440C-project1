a622bb4b3cf0449961bef7c7b83bed413826faa1
rarwrly <ehc2642@gmail.com>
2020-03-15 12:39:18 -0400

    updates to gallery

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*Mobile Navigation bar*/
-function myFunction() {
+function navFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -7,3 +7,49 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+/*---Index Page---*/
+/*Donation form  on Index*/
+ var submit = document.getElementById("donation-submit")
+
+submit.addEventListener('click', function(){
+	 	var donationName = document.getElementById("donation-full-name")
+ 		var donationAmt = document.getElementById("donation-amount")
+	
+	 		console.log(donationName)
+
+	 })
+
+/*Index page slide show*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+/*---- End Index Page*----*/
